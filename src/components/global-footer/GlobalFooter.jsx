82ab679e22d8b928210ed392ced4bd7edd57bba3
Mangaldip Mandal <mandalmangaldip11@gmail.com>
2025-04-09 00:49:10 +0530

    Update GlobalFooter.jsx

--- src/components/global-footer/GlobalFooter.jsx
+++ src/components/global-footer/GlobalFooter.jsx
@@ -41,9 +41,9 @@ const GlobalFooter = () => {
           </div>
         </div>
         <div className="text-center mt-10">
-          <p>Designed and styled by izoogood</p>
+          <p>Designed and styled by mangaldeeep</p>
           <p>
-            &copy; {new Date().getFullYear()} izoogood. All rights reserved.
+            &copy; {new Date().getFullYear()} mangaldeeep. All rights reserved.
           </p>
         </div>
       </div>
